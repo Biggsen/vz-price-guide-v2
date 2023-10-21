06e41e6130117af267393360a80a862ab8881c54
Biggs <biggs@devillion.com>
2023-10-21 10:43:45 +0100

    firebase config, vuefire strap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { VueFire } from 'vuefire'
+import { firebaseApp } from './firebase'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(VueFire, {
+	firebaseApp,
+	modules: []
+})
 
 app.use(router)
 
