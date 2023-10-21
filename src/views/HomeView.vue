4271776ce9611453e2927746f490e88f84f95b20
Biggs <biggs@devillion.com>
2023-10-21 10:44:42 +0100

    render collection data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useFirestore, useCollection } from 'vuefire'
+import { collection } from 'firebase/firestore'
+
+const db = useFirestore()
+
+const itemCollection = useCollection(collection(db, 'items'))
+console.log(itemCollection)
 </script>
 
 <template>
 	<main>
-		<TheWelcome />
+		<ul>
+			<li v-for="item in itemCollection" :key="item.id">{{ item.name }}</li>
+		</ul>
 	</main>
 </template>
