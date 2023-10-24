f0398daa9b8ead3a454da77e4f1c21e48854e9bc
Biggs <biggs@devillion.com>
2023-10-24 10:59:47 +0100

    listing in categories

---
+++ src/components/ItemTable.vue
@@ -0,0 +1,69 @@
+<script setup>
+const props = defineProps({
+	collection: {
+		type: Object
+	}
+})
+</script>
+
+<template>
+	<table class="table-auto">
+		<thead>
+			<tr>
+				<th rowspan="2" class="hidden">Material ID</th>
+				<th rowspan="2">Item/Block Name</th>
+				<th rowspan="2"></th>
+				<th colspan="2">Unit Price</th>
+				<th colspan="2">Stack Price</th>
+			</tr>
+			<tr>
+				<th>Buy</th>
+				<th>Sell</th>
+				<th>Buy</th>
+				<th>Sell</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="item in collection" :key="item.id">
+				<td class="hidden">{{ item.material_id }}</td>
+				<th width="50%" class="text-left">
+					<a
+						:href="item.url"
+						target="_blank"
+						class="font-normal hover:text-gray-asparagus"
+						>{{ item.name }}</a
+					>
+				</th>
+				<td width="5%">
+					<img :src="item.image" alt="" class="max-w-[50px]" />
+				</td>
+				<td class="text-center">{{ item.price }}</td>
+
+				<td class="text-center">{{ +parseFloat(item.price * 0.3).toFixed(2) }}</td>
+
+				<td class="text-center">{{ item.price * item.stack }}</td>
+				<td class="text-center">
+					{{ +parseFloat(item.price * 0.3 * item.stack).toFixed(2) }}
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+
+<style lang="scss" scoped>
+thead th {
+	@apply bg-gray-asparagus text-norway;
+}
+
+tbody {
+	th,
+	td {
+		@apply bg-norway;
+	}
+}
+
+th,
+td {
+	@apply border-2 border-white px-3 py-1;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,79 +1,73 @@
 <script setup>
 import { useFirestore, useCollection } from 'vuefire'
-import { query, collection, orderBy } from 'firebase/firestore'
+import { query, collection, orderBy, where } from 'firebase/firestore'
 
 import HeaderIntro from '../components/HeaderIntro.vue'
+import ItemTable from '../components/ItemTable.vue'
 
 const db = useFirestore()
 
-const q = query(collection(db, 'items'), orderBy('name'))
+const qDrops = query(
+	collection(db, 'items'),
+	where('category', '==', 'drops'),
+	orderBy('name', 'asc')
+)
+const qEarth = query(
+	collection(db, 'items'),
+	where('category', '==', 'earth'),
+	orderBy('name', 'asc')
+)
+const qFood = query(
+	collection(db, 'items'),
+	where('category', '==', 'food'),
+	orderBy('name', 'asc')
+)
+const qStone = query(
+	collection(db, 'items'),
+	where('category', '==', 'stone'),
+	orderBy('name', 'asc')
+)
+const qSand = query(
+	collection(db, 'items'),
+	where('category', '==', 'sand'),
+	orderBy('name', 'asc')
+)
+const qWood = query(
+	collection(db, 'items'),
+	where('category', '==', 'wood'),
+	orderBy('name', 'asc')
+)
+const qUtility = query(
+	collection(db, 'items'),
+	where('category', '==', 'utility'),
+	orderBy('name', 'asc')
+)
+const qOres = query(
+	collection(db, 'items'),
+	where('category', '==', 'ores'),
+	orderBy('name', 'asc')
+)
 
-const itemCollection = useCollection(q)
-console.log(itemCollection)
+const dropsCollection = useCollection(qDrops)
+const earthCollection = useCollection(qEarth)
+const foodCollection = useCollection(qFood)
+const stoneCollection = useCollection(qStone)
+const sandCollection = useCollection(qSand)
+const woodCollection = useCollection(qWood)
+const utilityCollection = useCollection(qUtility)
+const oresCollection = useCollection(qOres)
 </script>
 
 <template>
 	<main>
 		<HeaderIntro />
-		<p class="text-xl font-bold">{{ itemCollection.length }} items</p>
-		<table class="table-auto">
-			<thead>
-				<tr>
-					<th rowspan="2" class="hidden">Material ID</th>
-					<th rowspan="2">Item/Block Name</th>
-					<th rowspan="2"></th>
-					<th colspan="2">Unit Price</th>
-					<th colspan="2">Stack Price</th>
-				</tr>
-				<tr>
-					<th>Buy</th>
-					<th>Sell</th>
-					<th>Buy</th>
-					<th>Sell</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="item in itemCollection" :key="item.id">
-					<td class="hidden">{{ item.material_id }}</td>
-					<th width="50%" class="text-left">
-						<a
-							:href="item.url"
-							target="_blank"
-							class="font-normal hover:text-gray-asparagus"
-							>{{ item.name }}</a
-						>
-					</th>
-					<td width="5%">
-						<img :src="item.image" alt="" class="max-w-[50px]" />
-					</td>
-					<td class="text-center">{{ item.price }}</td>
-
-					<td class="text-center">{{ +parseFloat(item.price * 0.3).toFixed(2) }}</td>
-
-					<td class="text-center">{{ item.price * item.stack }}</td>
-					<td class="text-center">
-						{{ +parseFloat(item.price * 0.3 * item.stack).toFixed(2) }}
-					</td>
-				</tr>
-			</tbody>
-		</table>
+		<ItemTable :collection="oresCollection" />
+		<ItemTable :collection="stoneCollection" />
+		<ItemTable :collection="earthCollection" />
+		<ItemTable :collection="sandCollection" />
+		<ItemTable :collection="woodCollection" />
+		<ItemTable :collection="dropsCollection" />
+		<ItemTable :collection="foodCollection" />
+		<ItemTable :collection="utilityCollection" />
 	</main>
 </template>
-
-<style lang="scss" scoped>
-thead th {
-	@apply bg-gray-asparagus text-norway;
-}
-
-tbody {
-	th,
-	td {
-		@apply bg-norway;
-	}
-}
-
-th,
-td {
-	@apply border-2 border-white px-3 py-1;
-}
-</style>
