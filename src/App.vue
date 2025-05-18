b7512510b5331dfc1f6c479e0bfd2beaa4305c26
Biggs <biggsy@pm.me>
2025-05-19 00:26:45 +0100

    guarding routes and hiding nav links

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,8 @@ function signOutOfFirebase() {
 		<HeaderBanner />
 		<nav class="bg-gray-800 text-white px-4 py-2 flex gap-4 items-center">
 			<RouterLink class="hover:underline" to="/">Home</RouterLink>
-			<RouterLink class="hover:underline" to="/about">About</RouterLink>
-			<RouterLink class="hover:underline" to="/missing-items">Missing Items</RouterLink>
+			<RouterLink v-if="user?.email" class="hover:underline" to="/about">About</RouterLink>
+			<RouterLink v-if="user?.email" class="hover:underline" to="/missing-items">Missing Items</RouterLink>
 			<RouterLink v-if="user?.email" class="hover:underline" to="/add">Add Item</RouterLink>
 			<RouterLink v-if="!user?.email" class="hover:underline ml-auto" to="/login">Login</RouterLink>
 			<button v-else class="hover:underline ml-auto" @click="signOutOfFirebase">Logout</button>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { getCurrentUser } from 'vuefire'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,19 +23,32 @@ const router = createRouter({
 		{
 			path: '/add',
 			name: 'add',
-			component: () => import('../views/AddItemView.vue')
+			component: () => import('../views/AddItemView.vue'),
+			meta: { requiresAuth: true }
 		},
 		{
 			path: '/edit/:id',
 			name: 'edit',
-			component: () => import('../views/EditItemView.vue')
+			component: () => import('../views/EditItemView.vue'),
+			meta: { requiresAuth: true }
 		},
 		{
 			path: '/missing-items',
 			name: 'missing-items',
-			component: () => import('../views/MissingItemsView.vue')
+			component: () => import('../views/MissingItemsView.vue'),
+			meta: { requiresAuth: true }
 		}
 	]
+})
+
+router.beforeEach(async (to, from, next) => {
+	if (to.meta.requiresAuth) {
+		const user = await getCurrentUser()
+		if (!user) {
+			return next({ path: '/login', query: { redirect: to.fullPath } })
+		}
+	}
+	next()
 })
 
 export default router
