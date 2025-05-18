0e42923438c5fe017b4f0e946c74325deae0511c
Biggs <biggsy@pm.me>
2025-05-18 12:04:45 +0100

    simple nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { useCurrentUser } from 'vuefire'
+import { useCurrentUser, useFirebaseAuth } from 'vuefire'
+import { signOut } from '@firebase/auth'
 
 import HeaderBanner from './components/HeaderBanner.vue'
 
 const user = useCurrentUser()
+const auth = useFirebaseAuth()
+
+function signOutOfFirebase() {
+	signOut(auth)
+}
 </script>
 
 <template>
 	<header>
 		<HeaderBanner />
-		<!-- <div class="wrapper">
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
-				<RouterLink v-if="user?.email" to="/newitem">New item</RouterLink>
-			</nav>
-		</div> -->
+		<nav class="bg-gray-800 text-white px-4 py-2 flex gap-4 items-center">
+			<RouterLink class="hover:underline" to="/">Home</RouterLink>
+			<RouterLink class="hover:underline" to="/about">About</RouterLink>
+			<RouterLink class="hover:underline" to="/missing-items">Missing Items</RouterLink>
+			<RouterLink v-if="user?.email" class="hover:underline" to="/add">Add Item</RouterLink>
+			<RouterLink v-if="!user?.email" class="hover:underline ml-auto" to="/login">Login</RouterLink>
+			<button v-else class="hover:underline ml-auto" @click="signOutOfFirebase">Logout</button>
+		</nav>
 	</header>
 
 	<RouterView />
