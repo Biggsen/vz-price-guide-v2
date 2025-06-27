8b28020920982cc958ef91156c285a775cf61cb5
Biggs <biggsy@pm.me>
2025-06-27 11:31:48 +0100

    header and toc components. sections restructure and cleanup.

---
+++ src/components/PrivacyPolicyTOC.vue
@@ -0,0 +1,159 @@
+<template>
+	<div>
+		<div id="toc">
+			<strong>
+				<span>
+					<h2>TABLE OF CONTENTS</h2>
+				</span>
+			</strong>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#infocollect">
+					<span>1. WHAT INFORMATION DO WE COLLECT?</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#infouse">
+					<span>2. HOW DO WE PROCESS YOUR INFORMATION?</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#legalbases">
+					<span>
+						3. WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR PERSONAL INFORMATION?
+					</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<span>
+				<span>
+					<a data-custom-class="link" href="#whoshare">
+						4. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+					</a>
+				</span>
+			</span>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#cookies">
+					<span>5. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</span>
+				</a>
+			</span>
+		</div>
+
+		<div>
+			<span>
+				<a data-custom-class="link" href="#sociallogins">
+					<span>
+						<span>
+							<span>6. HOW DO WE HANDLE YOUR SOCIAL LOGINS?</span>
+						</span>
+					</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#inforetain">
+					<span>7. HOW LONG DO WE KEEP YOUR INFORMATION?</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#infosafe">
+					<span>8. HOW DO WE KEEP YOUR INFORMATION SAFE?</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<span>
+				<span>
+					<a data-custom-class="link" href="#privacyrights">
+						9. WHAT ARE YOUR PRIVACY RIGHTS?
+					</a>
+				</span>
+			</span>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#DNT">
+					<span>10. CONTROLS FOR DO-NOT-TRACK FEATURES</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<span>
+				<a data-custom-class="link" href="#uslaws">
+					<span>11. DO UNITED STATES RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<a data-custom-class="link" href="#otherlaws">
+				<span>12. DO OTHER REGIONS HAVE SPECIFIC PRIVACY RIGHTS?</span>
+			</a>
+		</div>
+
+		<div>
+			<span>
+				<a data-custom-class="link" href="#policyupdates">
+					<span>13. DO WE MAKE UPDATES TO THIS NOTICE?</span>
+				</a>
+			</span>
+		</div>
+		<div>
+			<a data-custom-class="link" href="#contact">
+				<span>14. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</span>
+			</a>
+		</div>
+		<div>
+			<a data-custom-class="link" href="#request">
+				<span>15. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</span>
+			</a>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+[data-custom-class='body'],
+[data-custom-class='body'] * {
+	background: transparent !important;
+}
+[data-custom-class='title'],
+[data-custom-class='title'] * {
+	font-size: 26px !important;
+	color: #000000 !important;
+}
+[data-custom-class='subtitle'],
+[data-custom-class='subtitle'] * {
+	color: #595959 !important;
+	font-size: 14px !important;
+}
+[data-custom-class='heading_1'],
+[data-custom-class='heading_1'] * {
+	font-size: 19px !important;
+	color: #000000 !important;
+}
+[data-custom-class='heading_2'],
+[data-custom-class='heading_2'] * {
+	font-size: 17px !important;
+	color: #000000 !important;
+}
+[data-custom-class='body_text'],
+[data-custom-class='body_text'] * {
+	color: #595959 !important;
+	font-size: 16px !important;
+}
+[data-custom-class='link'],
+[data-custom-class='link'] * {
+	font-size: 16px !important;
+	word-break: break-word !important;
+}
+</style>
