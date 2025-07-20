aeb89e306e071ee87a991d64b6ffe0a1e3fd07ba
Biggs <biggsy@pm.me>
2025-07-20 22:05:48 +0100

    footer simplified

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,19 @@
 <template>
-	<footer class="bg-heavy-metal p-3">
-		<a href="#top" class="text-norway underline block mb-4 text-sm">Back to top</a>
-		<p class="text-norway text-sm mb-2">© Copyright 2025</p>
-		<p class="text-norway text-sm mb-2">
-			minecraft-economy-price-guide.net is not affiliated with
-			<a href="https://minecraft.net/" class="underline">Mojang</a>
-		</p>
+	<footer class="p-3 mt-12">
 		<div class="flex gap-4 text-sm">
-			<router-link to="/privacy-policy" class="text-norway underline">
-				Privacy Policy
+			<p class="text-heavy-metal text-sm">© 2025</p>
+			<router-link to="/privacy-policy" class="text-heavy-metal underline">
+				<span class="sm:hidden">Privacy</span>
+				<span class="hidden sm:inline">Privacy Policy</span>
 			</router-link>
-			<router-link to="/cookie-policy" class="text-norway underline">
-				Cookie Policy
+			<router-link to="/cookie-policy" class="text-heavy-metal underline">
+				<span class="sm:hidden">Cookies</span>
+				<span class="hidden sm:inline">Cookie Policy</span>
 			</router-link>
-			<router-link to="/terms-of-use" class="text-norway underline">Terms of Use</router-link>
+			<router-link to="/terms-of-use" class="text-heavy-metal underline">
+				<span class="sm:hidden">Terms</span>
+				<span class="hidden sm:inline">Terms of Use</span>
+			</router-link>
 		</div>
 	</footer>
 </template>
