eb947e2931211ecbb6178feca2034f77494bd976
Biggs <biggsy@pm.me>
2025-07-20 16:01:44 +0100

    header updated for mobile

--- src/components/HeaderBanner.vue
+++ src/components/HeaderBanner.vue
@@ -6,8 +6,10 @@ import { CubeIcon } from '@heroicons/vue/24/solid'
 	<div class="bg-gray-asparagus p-4 sm:p-8 pt-3 sm:pt-7 pattern">
 		<h1
 			class="flex items-center gap-2 text-lg sm:text-2xl text-white font-bold [text-shadow:_2px_2px_2px_#000]">
-			<CubeIcon class="w-7 h-7 sm:w-8 sm:h-8 text-white drop-shadow mt-1" />
-			verzion's economy price guide for Minecraft
+			<CubeIcon
+				class="w-7 h-7 sm:w-8 sm:h-8 text-white drop-shadow mt-1 [filter:_drop-shadow(2px_2px_2px_#000)]" />
+			<span class="block sm:hidden">vz's price guide for Minecraft</span>
+			<span class="hidden sm:block">verzion's economy price guide for Minecraft</span>
 		</h1>
 	</div>
 </template>
