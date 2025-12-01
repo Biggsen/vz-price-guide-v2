eb45c8dc138af0a0144ef73291ed98afc62fef1d
Biggs <biggsy@pm.me>
2025-12-01 23:32:10 +0000

    changing header h1 to a span

--- src/components/HeaderBanner.vue
+++ src/components/HeaderBanner.vue
@@ -4,12 +4,12 @@ import { CubeIcon } from '@heroicons/vue/24/solid'
 
 <template>
 	<div class="bg-gray-asparagus p-4 sm:p-8 pt-3 sm:pt-7 pattern hidden sm:block">
-		<h1
+		<div
 			class="flex items-center gap-2 text-lg sm:text-2xl text-white font-bold [text-shadow:_2px_2px_2px_#000]">
 			<CubeIcon
 				class="w-7 h-7 sm:w-8 sm:h-8 text-white drop-shadow mt-1 [filter:_drop-shadow(2px_2px_2px_#000)]" />
 			<span class="block">verzion's economy price guide for Minecraft</span>
-		</h1>
+		</div>
 	</div>
 </template>
 
