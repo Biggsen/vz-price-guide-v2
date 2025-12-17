14ad9c575471b98acff83da730fd36c6ebf341ec
Biggs <biggsy@pm.me>
2025-12-17 01:07:14 +0000

    Add h1 heading to homepage in HeaderBanner component

--- src/components/HeaderBanner.vue
+++ src/components/HeaderBanner.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { CubeIcon } from '@heroicons/vue/24/solid'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const isHomepage = route.path === '/'
 </script>
 
 <template>
@@ -8,7 +12,8 @@ import { CubeIcon } from '@heroicons/vue/24/solid'
 			class="flex items-center gap-2 text-lg sm:text-2xl text-white font-bold [text-shadow:_2px_2px_2px_#000]">
 			<CubeIcon
 				class="w-7 h-7 sm:w-8 sm:h-8 text-white drop-shadow mt-1 [filter:_drop-shadow(2px_2px_2px_#000)]" />
-			<span class="block">verzion's economy price guide for Minecraft</span>
+			<h1 v-if="isHomepage" class="block">verzion's economy price guide for Minecraft</h1>
+			<span v-else class="block">verzion's economy price guide for Minecraft</span>
 		</div>
 	</div>
 </template>
